0690d29f6df4bc06a6efeb178ced80909e84b33e
dalietnguyen342 <92266179+dalietnguyen342@users.noreply.github.com>
2022-09-08 12:39:29 +0700

    Delete Untitled1.cpp

--- Untitled1.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main () {
-  cout << "Bai tap nhom KNNN" << endl;
-  cout << "Nhom: Hoi Du Hoc Sinh Sai Gon" << endl;
-  system("Pause");
-  return 0;
-}
